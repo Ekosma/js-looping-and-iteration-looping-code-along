943842a27ba70d46ac27cdc0f1880843306194a2
Evelyn <evelyn.a.kosma@gmail.com>
2021-12-18 13:55:29 -0600

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+//writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(name, event) {
+  let arr = []
+  for (let i = 0; i < name.length; i++) {
+      arr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`) 
+    } 
+  return arr
+}
+
+
+function countDown(n) {
+  for (let i = n; i >= 0; i--) {
+    console.log(i)
+  }  
+}
